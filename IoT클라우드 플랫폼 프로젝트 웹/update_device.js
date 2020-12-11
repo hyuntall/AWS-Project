5fe16896d131d14040d379cf7e7e84c48fa078f3
hyuntall <71054445+hyuntall@users.noreply.github.com>
2020-12-12 03:37:35 +0900

    Update update_device.js

--- IoT클라우드 플랫폼 프로젝트 웹/update_device.js
+++ IoT클라우드 플랫폼 프로젝트 웹/update_device.js
@@ -5,7 +5,7 @@ var payload;
 function On() {
 
     document.getElementById("result").innerHTML="조회 중...";
- payload = {
+ payload = { // 다음의 내용을 payload에 입력한다. 호출되는 람다함수는 timeWT라는 태그네임이 입력되었을 때에만 날씨api로부터 날씨 정보를 얻어와 디바이스 섀도우에 전송한다.
     tags : [
             {
                 tagName: "timeWT",
@@ -18,7 +18,7 @@ function On() {
 }
 // API 중지
 
-var invokeAPI = function() {
+var invokeAPI = function() { //다음 API 주소에 payload를 전송한다.
     // 디바이스 조회 URI
     var API_URI = 'https://q1gtpjku33.execute-api.ap-northeast-2.amazonaws.com/prod/devices/MyMKRWiFi1010';
     
@@ -26,9 +26,9 @@ var invokeAPI = function() {
     $.ajax(API_URI, {
         method: 'PUT',
         contentType: "application/json",
-        data: JSON.stringify(payload),
+        data: JSON.stringify(payload), // payload를 전송하기 전에 문자열로 바꿔준다.
 
-        success: function (data, status, xhr) {
+        success: function (data, status, xhr) { // API에 payload 전송에 성공했을 때, 호출된 람다함수에서 얻은 현재 날씨 정보를 웹 화면에 출력한다.
                 var result = JSON.parse(data);
                 document.getElementById("result").innerText = "현재 기온: "+ result.state.desired.WT+"ºC";
                
