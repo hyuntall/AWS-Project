cc52f30f3fb40bfd353ce597f30c123aed8bc8a3
hyuntall <71054445+hyuntall@users.noreply.github.com>
2020-12-12 15:45:25 +0900

    Add files via upload

---
+++ LogDeviceLambdaJavaProject/src/main/java/com/amazonaws/lambda/demo/LogDeviceHandler.java
@@ -0,0 +1,85 @@
+package com.amazonaws.lambda.demo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.TimeZone;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LogDeviceHandler implements RequestHandler<Event, String> {
+
+    private DynamoDB dynamoDb;
+    private String DYNAMODB_TABLE_NAME = "Logging";
+
+    @Override
+    public String handleRequest(Event input, Context context) {
+        this.initDynamoDbClient();
+
+        Table table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
+
+        long from=0;
+        long to=0;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+            from = sdf.parse(input.from).getTime() / 1000;
+            to = sdf.parse(input.to).getTime() / 1000;
+        } catch (ParseException e1) {
+            e1.printStackTrace();
+        }
+
+        QuerySpec querySpec = new QuerySpec()
+                .withKeyConditionExpression("deviceId = :v_id and #t between :from and :to")
+                .withNameMap(new NameMap().with("#t", "time"))
+                .withValueMap(new ValueMap().withString(":v_id",input.device).withNumber(":from", from).withNumber(":to", to)); 
+
+        ItemCollection<QueryOutcome> items=null;
+        try {           
+            items = table.query(querySpec);
+        }
+        catch (Exception e) {
+            System.err.println("Unable to scan the table:");
+            System.err.println(e.getMessage());
+        }
+
+        return getResponse(items);
+    }
+
+    private String getResponse(ItemCollection<QueryOutcome> items) {
+
+        Iterator<Item> iter = items.iterator();
+        String response = "{ \"data\": [";
+        for (int i =0; iter.hasNext(); i++) {
+            if (i!=0) 
+                response +=",";
+            response += iter.next().toJSON();
+        }
+        response += "]}";
+        return response;
+    }
+
+    private void initDynamoDbClient() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+
+        this.dynamoDb = new DynamoDB(client);
+    }
+}
+
+class Event {
+    public String device;
+    public String from;
+    public String to;
+}
